06b37eaba5eac5434fe8d8e271f99fb1e43513d7
Nehmulos <nehmu@lavabit.com>
2012-12-16 01:22:00 +0100

    ehh yeah I start remembering basic math again, so I would call it progress. Headache midnightdev commit

--- src/Camera.js
+++ src/Camera.js
@@ -18,8 +18,7 @@ Camera.inherit(Object, {
                 this.game.setMap(this.trackedActor.map);
             }
             
-            this.game.map.position.x = -this.trackedActor.position.x + this.size.width/2;
-            this.game.map.position.y = -this.trackedActor.position.y + this.size.height/2;
+            this.centerAt(this.trackedActor.position);
         }
         
         
@@ -47,21 +46,47 @@ Camera.inherit(Object, {
     },
     
     scaleByDelta: function(delta, event) {
-        console.log("scroll"+delta);
+        
         var scaleDelta = delta * 0.0005;
         var newScale = this.game.map.scale + scaleDelta;
         if (newScale > 0) {
             this.game.map.scale = newScale;
             if (!this.trackedActor) {
-                var mousePoint = mouseToCamera(new cc.Point(event.pageX, event.pageY));
-                this.centerAt(mousePoint);
+                var mousePoint = this.mouseToCamera(new cc.Point(event.pageX, event.pageY));
+                //this.centerAt(mousePoint);
             }
+            console.log(this.game.map.scale)
         }
     },
     
     mouseToCamera: function(point) {
-        return new cc.Point(point.x - this.game.map.position.x,
-                            point.y - this.game.map.position.y)
+/*
+ __        _______     __  __    _  _____ _   _     _   _  ___    ___        __
+ \ \      / / ____|   |  \/  |  / \|_   _| | | |   | \ | |/ _ \  / \ \      / /
+  \ \ /\ / /|  _|     | |\/| | / _ \ | | | |_| |   |  \| | | | |/ _ \ \ /\ / / 
+   \ V  V / | |___    | |  | |/ ___ \| | |  _  |   | |\  | |_| / ___ \ V  V /  
+    \_/\_/  |_____|   |_|  |_/_/   \_\_| |_| |_|   |_| \_|\___/_/   \_\_/\_/  
+*/
+        var p = new cc.Point(point.x, point.y)
+        p.x -= this.size.width/2;
+        p.x /= this.game.map.scale;
+        p.x += this.size.width/2; // fuck yeah I've got it
+        
+        p.y -= this.size.height/2;
+        p.y /= this.game.map.scale;
+        p.y += this.size.height/2; // fuck yeah I've got it
+        
+
+        
+        p.x -= (this.game.map.position.x/this.game.map.scale);
+        p.y -= (this.game.map.position.y/this.game.map.scale);
+        return p;
+        
+    },
+    
+    centerAt: function(point) {
+        this.game.map.position.x = (-point.x + this.size.width/2)*this.game.map.scale;
+        this.game.map.position.y = (-point.y + this.size.height/2)*this.game.map.scale;
     }
 });
 
--- src/Game.js
+++ src/Game.js
@@ -4,30 +4,28 @@ function Game(root) {
     this.ui = new Ui();
     
     // test game
-    /*
-    this.map = new Map();
+    var map = new Map();
     var nodes = new Array();
     nodes.push(new Node( 100, 100));
     nodes.push(new Node( 400, 100, [nodes[0]]));
     nodes.push(new Node( 700, 100, [nodes[1]]));
     nodes.push(new Node( 400, 400, [nodes[1]]));
-    this.map.setNodes(nodes);
+    map.setNodes(nodes);
     
     var sprite = new cc.Sprite({file: "images/house.png"});
     sprite.position = new cc.Point(100, 200);
-    this.map.addSprite(sprite);
+    map.addSprite(sprite);
     
     var actor = new Actor(nodes[3]);
-    this.map.addActor(actor);
+    map.addActor(actor);
     actor.addNodeToPath(nodes[1]);
     actor.addNodeToPath(nodes[0]);
     actor.addNodeToPath(nodes[1]);
     actor.addNodeToPath(nodes[3]);
     
-    this.map.restCamera();
-    root.addChild(this.map);
-    */
-    this.setMap(new TownGenerator().create());
+    this.setMap(map);
+
+    //this.setMap(new TownGenerator().create());
     this.camera = new Camera(cc.Director.sharedDirector.winSize, this);
     //this.camera.trackedActor = actor;
 }
@@ -62,7 +60,8 @@ Game.inherit(Object, {
     },
     
     mouseUp: function(event) {
-        var point = mouseToCamera(event.locationInCanvas);
+        var point = this.camera.mouseToCamera(event.locationInCanvas);
+        console.log(""+event.locationInCanvas.x+" "+event.locationInCanvas.y+" be " +point.x +" "+ point.y)
         var actor = this.map.getEntityOnPosition(point, "actor");
         var building = this.map.getEntityOnPosition(point, "building");
     
