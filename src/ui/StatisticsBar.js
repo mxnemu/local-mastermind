9bbdf4f849dee4878b22269196bcf3cc85171ea6
Nehmulos <nehmu@lavabit.com>
2013-02-14 21:58:45 +0100

    set henchmenoverlay on buttonclick;

--- src/ui/StatisticsBar.js
+++ src/ui/StatisticsBar.js
@@ -4,8 +4,13 @@ function StatisticsBar(ui) {
 
 StatisticsBar.inherit(Object, {
     init: function() {
+        var _this = this;
         if (this.ui.player.henchmen.length > 0) {
             $(".showHenchmenButton").show("slow");
         }
+        
+        $(".showHenchmenButton").click(function() {
+            ui.overlay = new OverlayListHenchmen(_this.ui.player.henchmen);
+        });
     }
 });
