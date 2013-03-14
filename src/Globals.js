fc744b586699b3e1f22817710f2a37192f9b69ca
Nehmulos <nehmu@lavabit.com>
2013-03-14 21:55:53 +0100

    use doubleclick to enter bulidings; use double rightclick on empty space to leave buildings;

--- src/Globals.js
+++ src/Globals.js
@@ -2,5 +2,9 @@ window.G = {}
 window.G.debug = window.location.hash.match(/#debug/) ? true : false;
 window.G.noSound = window.location.hash.match(/#noSound/) ? true : false;
 
+window.G.leftMouseButtonIndex = 1;
+window.G.rightMouseButtonIndex = 3;
+window.G.doubleClickTime = 300;
+
 window.G.connectionLineZ = -1;
 window.G.interiorZ = -2;
