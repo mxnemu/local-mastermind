7d0133c189f9f2f1ffdbccd9c42b40fcac3e3e3e
Nehmulos <nehmu@lavabit.com>
2013-04-25 02:05:42 +0200

    made pages invisible; added VERY SIMPLE INCOMPLETE navigation buttons to switch pages;

--- src/pages/CreatePlayer.js
+++ src/pages/CreatePlayer.js
@@ -7,6 +7,31 @@ $(function() {
     var lastName = null;
     var honorificTitle = null;
     
+    var status = {
+        pageId:0    
+    };
+    
+    var loadPage = function(id) {
+        var pages = $(".screen");
+        if (id < 0 || id >= pages.length) {
+            return;
+        }
+    
+        status.pageId = id;
+        $(".screen").hide("slow");
+        $(pages.get(id)).show("slow");
+    };
+    
+    $(".previousPageButton").click(function() {
+        console.log("prev");
+        loadPage(status.pageId-1);
+    });
+    
+    $(".nextPageButton").click(function() {
+        console.log("next");
+        loadPage(status.pageId+1);
+    });
+    
     $("input[type=radio][name=archtype]").change(function() {
         archtype = this.value;
         $(".archtypeText").text(archtype);
