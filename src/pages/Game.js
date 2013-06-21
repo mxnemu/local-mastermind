6e6f711895266f8f89e3d7365d0301705fa2e9b4
Nehmulos <nehmu@lavabit.com>
2013-06-21 22:39:13 +0200

    restore small canvas size when leaving the fullscreen mode;

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -9,16 +9,35 @@ $(function() {
                       function() {};
         reqFscr.call(element);
         
-        element.classList.add("fullscreen");
-        
-        // shitty timeout workaround for css computation time
-        setTimeout(function() {
+        function resizeCanvas() {
             var director = cc.Director.sharedDirector;
             var cocosElement = document.getElementById('cocos2d-demo');
             cocosElement.innerHTML = "";
             director.attachInView(cocosElement);
             Application.instance.bindCanvasEvents();
             Application.instance.game.camera.size = director.winSize;
-        }, 200);
+        }
+        
+        function leaveFullScreen() {
+            var caller = arguments.callee;
+            document.removeEventListener("webkitfullscreenchange", caller, false);
+            document.removeEventListener("mozfullscreenchange", caller, false);
+            document.removeEventListener("fullscreenchange", caller, false);
+        
+            element.classList.remove("fullscreen");
+            setTimeout(resizeCanvas, 200);
+        }
+        
+        function enterFullScreen() {
+            element.classList.add("fullscreen");
+            document.addEventListener("webkitfullscreenchange", leaveFullScreen);
+            document.addEventListener("mozfullscreenchange", leaveFullScreen);
+            document.addEventListener("fullscreenchange", leaveFullScreen);
+            
+            resizeCanvas();
+        }
+
+        // shitty timeout workaround for css computation time
+        setTimeout(enterFullScreen, 200);
     });
 });
